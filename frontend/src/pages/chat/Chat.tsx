30b813c70b59ff0c70004b7f87853d0dba6cb394
Scott Kirkland <srkirkland@gmail.com>
2023-10-30 10:16:19 -0700

    Update Chat message

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -548,8 +548,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>What would you like to know?</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is trained on the UC Davis public knowledge base and is ready to answer your questions</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
